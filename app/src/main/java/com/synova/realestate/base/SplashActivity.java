a38aa7a8b3f94b718a120283e9f25e84a1b0a8b2
Tran Huu Duc <thduc89@gmail.com>
2015-06-20 08:15:13 +0700

    + Fix bug crash on press back key in Splash screen

--- app/src/main/java/com/synova/realestate/base/SplashActivity.java
+++ app/src/main/java/com/synova/realestate/base/SplashActivity.java
@@ -1,6 +1,7 @@
 
 package com.synova.realestate.base;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,7 +11,7 @@ import com.synova.realestate.R;
 /**
  * Created by ducth on 6/17/15.
  */
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +24,10 @@ public class SplashActivity extends BaseActivity {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
         }, 1500);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Prevent press back key
     }
 }
